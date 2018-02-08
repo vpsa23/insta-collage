aafb7817a7277de14ab6e266702aa64d2c5487d1
vpsa23 <vpsa23@hotmail.com>
2018-02-08 09:04:10 -0300

    Arreglando Funcion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,59 +1,15 @@
-var container;
-var elementoMove;
-window.addEventListener('load', init);
-
-container = document.querySelector('.square');
-container.addEventListener('dragover', dragSobreContainer, false);
-container.addEventListener('dragleave', dragSalioContainer, false);
-container.addEventListener('drog', manejandoDrog, false);
-
-function init (){
-
-  
-  var imagenes = document.getElementsByClassName('img');
-  for (i in imagenes){
-    var imag = imagenes[i];
-    var x = random(0, 90);
-    var y = random (0, 90);
-
-    if (typeof imag.style != 'undefined'){
-      imag.style.top = y+'%';
-      imag.style.left = x+'%';
-      imag.addEventListener('dragstart', dragIniciado, false);
-      imag.addEventListener('dragend', dragFinalizado, false);
-
-    }
-  }
+function drag(e){
+  console.log(e.target.src)
+  e.dataTransfer.setData('text', e.target.id);
 }
-function dragIniciado(e){
-  this.style.backgroundColor= 'blue';
-  elementoMove = this;
-  var father = document.createElement('p');
-  var clone = this.cloneNode(true);
-  father.appendChild(clone);
-  e.dataTransfer.setData('text', father.innerHTML)
 
-}
-function manejandoDrog(e){
+function permitirDrop(e){
   e.preventDefault();
-  var datos = e.dataTransfer.getData('text');
-  elementoMove.parentNode.removeChild(elementoMove);
-  this.innerHTML += datos;
 }
-function dragSobreContainer(e){
+
+function drop(e){
   e.preventDefault();
-  this.classList.add('over');
-  return false;
-}
-function dragSalioContainer(e){
-  this.classList.remove('over');
+  var photo = e.dataTransfer.getData('text');
+  e.target.appendChild(document.getElementById(photo));
 }
 
-
-function dragFinalizado(e){
-  this.style.backgroundColor = 'red';
-}
-
-function random (min, max){
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
